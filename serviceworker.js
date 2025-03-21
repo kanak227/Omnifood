f3d86a5090e6b1772ee6dc34819952cfd7678534
kanak227 <kanakverma325@gmail.com>
2025-03-21 22:04:22 +0530

    changes

--- serviceworker.js
+++ serviceworker.js
@@ -1,13 +1,37 @@
 const CACHE_NAME = "app-cache-v1";
 const urlsToCache = [
-    "/",
-    "/index.html",
-    "/css/style.css",
-    "/css/general.css",
-    "/css/queries.css",
-    "/js/script.js",
-    "/img/favicon-192.webp",
-    "/img/favicon-512.webp"
+    '/',
+    '/index.html',
+    '/css/general.css',
+    '/css/style.css',
+    '/css/queries.css',
+    '/js/script.js',
+    '/img/omnifood-logo.avif',
+    '/img/hero-min.webp',
+    '/img/customers/customer-1.avif',
+    '/img/customers/customer-2.avif',
+    '/img/customers/customer-3.avif',
+    '/img/customers/customer-4.avif',
+    '/img/customers/customer-5.avif',
+    '/img/customers/customer-6.avif',
+    '/img/meals/meal-1.avif',
+    '/img/meals/meal-2.avif',
+    '/img/gallery/gallery-1.avif',
+    '/img/gallery/gallery-2.avif',
+    '/img/gallery/gallery-3.avif',
+    '/img/gallery/gallery-4.avif',
+    '/img/gallery/gallery-5.avif',
+    '/img/gallery/gallery-6.avif',
+    '/img/gallery/gallery-7.avif',
+    '/img/gallery/gallery-8.avif',
+    '/img/gallery/gallery-9.avif',
+    '/img/gallery/gallery-10.avif',
+    '/img/gallery/gallery-11.avif',
+    '/img/gallery/gallery-12.avif',
+    '/img/meals/meal-3.avif', 
+    '/img/meals/meal-4.avif', 
+    '/img/meals/meal-5.avif', 
+    '/img/meals/meal-6.avif'  
 ];
 
 //Install Service Worker and Cache Static Files
@@ -21,7 +45,7 @@ self.addEventListener("install", (event) => {
 });
 
 //Fetch and Serve Cached Files
-    self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((cachedResponse) => {
         return cachedResponse || fetch(event.request);
@@ -30,7 +54,7 @@ self.addEventListener("install", (event) => {
 });
 
 //Activate and Clean Up Old Caches
-    self.addEventListener("activate", (event) => {
+self.addEventListener("activate", (event) => {
     event.waitUntil(
         caches.keys().then((cacheNames) => {
         return Promise.all(
